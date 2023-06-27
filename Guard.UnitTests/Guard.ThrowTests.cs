d4c13d3212ceff56b77134fb917951b1f2698fc5
Fatih AYLAN <fatih.aylan@bilgeadam.com>
2023-06-27 18:25:26 +0300

    Throw extensions and tests added

---
+++ Guard.UnitTests/Guard.ThrowTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guard.UnitTests
+{
+    public class GuardThrowTests
+    {
+        [Fact]
+        public void Throw_WhenGenericTypeIsDefined_ExceptionIsCaughtInAction_ShouldThrowWithGenericType()
+        {
+            Assert.Throws<ArithmeticException>(() => Guard.Throw<ArithmeticException>(() => { Guard.ThrowIfLessThan(1, 2, "value"); }));
+        }
+
+        [Fact]
+        public void Throw_WhenGenericTypeIsDefined_MessageParameterNotNull_ExceptionIsCaughtInAction_ShouldThrowWithGenericTypeAndCustomMessage()
+        {
+            string message = "Throw an ArithmeticException";
+            var exception = Assert.Throws<ArithmeticException>(() => Guard.Throw<ArithmeticException>(() => { Guard.ThrowIfLessThan(1, 2, "value"); }, message));
+            Assert.Equal(message, exception.Message);
+        }
+
+        [Fact]
+        public void Throw_WhenGenericTypeIsNotDefined_ExceptionIsCaughtInAction_ShouldThrow()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.Throw(() => { Guard.ThrowIfLessThan(1, 2, "value"); }));
+        }
+
+        [Fact]
+        public void Throw_WhenGenericTypeIsDefined_NoExceptionIsCaughtInAction_ShouldNotThrow()
+        {
+            Guard.Throw<ArithmeticException>(() => { Guard.ThrowIfLessThan(3, 2, "value"); });
+        }
+
+        [Fact]
+        public void Throw_WhenGenericTypeIsDefined_MessageParameterNotNull_NoExceptionIsCaughtInAction_ShouldNotThrow()
+        {
+            string message = "Throw an ArithmeticException";
+            Guard.Throw<ArithmeticException>(() => { Guard.ThrowIfLessThan(3, 2, "value"); }, message);
+        }
+
+        [Fact]
+        public void Throw_WhenGenericTypeIsNotDefined_NoExceptionIsCaughtInAction_ShouldNotThrow()
+        {
+            Guard.Throw(() => { Guard.ThrowIfLessThan(3, 2, "value"); });
+        }
+    }
+}
---
+++ Guard/Guard.Throw.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guard
+{
+    public static partial class Guard
+    {
+        public static void Throw(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void Throw<TException>(Action action, string? message = null) where TException : Exception
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                throw (TException)Activator.CreateInstance(typeof(TException), message ?? ex.Message, ex);
+            }
+        }
+    }
+}
